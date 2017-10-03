cb9833a8c5008a3cbc094e3e79df9137dda4943d
ricardo1447 <rflores42@mail.csuchico.edu>
2017-10-02 17:47:45 -0700

    Added comments for functional decomposition

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "bitmap.h"
+#include <vectors>
+#include <strings>
+
+int main()
+{
+
+// Output will request the image file name from the user.
+// Image file must be a Windows BMP format image.
+// Change the pixel colors to black, white, and shades of grey only based on original colors.
+// File must be saved as oldtimey.bmp
+    return 0;
+
+}
