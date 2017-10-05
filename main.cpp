302f41413a2ec08b2eef69e14e089cbd1791a514
ricardo1447 <rflores42@mail.csuchico.edu>
2017-10-04 18:14:30 -0700

    Outputs size of pixel matrix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
 #include "bitmap.h"
-#include <vectors>
-#include <strings>
+#include <vector>
+#include <string>
+
+using namespace std;   
 
 int main()
 {
+    Bitmap image;
+    vector < vector < Pixel > > bmp;
+    Pixel rgb;
+
+    image.open("machupicchu.bmp");
+    bmp = image.toPixelMatrix();
+
+
+    cout << "machupicchu.mbp has been loaded. It is " << bmp.size() << " pixels wide and " << bmp[0].size() << " pixels high" << endl;
 
 // Output will request the image file name from the user.
 // Image file must be a Windows BMP format image.
