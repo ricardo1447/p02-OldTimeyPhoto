8c8e0aa0c6bee18f43d4242d3607f9e62d9f3214
ricardo1447 <rflores42@mail.csuchico.edu>
2017-10-16 18:04:45 -0700

    Creates old timey file

--- main.cpp
+++ main.cpp
@@ -7,29 +7,39 @@ using namespace std;
 
 int main()
 {
-    Bitmap image;
-    vector < vector < Pixel > > bmp;
-    Pixel rgb;
+        Bitmap image;
+        vector < vector < Pixel > > bmp;
+        Pixel rgb;
 
-    string input;
-    cout << "Please state the file name of an image: " << endl;
-    cin >> input;
+        string input;
+        cout << "Please state the file name of an image: " << endl;
+        cin >> input;
 
-    image.open(input);
+        image.open(input);
 
-    bool validBmp = image.isImage();
-    if(validBmp == true)
-    {
-    bmp = image.toPixelMatrix();
+        bool validBmp = image.isImage();
+        if(validBmp == true)
+        {
+                cout<<"Opened image\n";
+                bmp = image.toPixelMatrix();
+                for(int row = 0; row < bmp.size(); row ++)
+                {
+                        for(int col = 0; col < bmp[0].size(); col ++)
+                        {
+                                rgb.red = 0;
+                                rgb.blue = 0;
+                                rgb.green = 0;
 
-    rgb.red = 0;
-    rgb.blue = 0;
-    rgb.green = 0;
-    
-    bmp[bmp.size() - 1][bmp[0].size() - 1] = rgb;
-    image.fromPixelMatrix(bmp);
-    image.save("machupicchu.bmp");
-    }
-    return 0;
+                                bmp[row][col] = rgb;
+                        }
+                }
+                image.fromPixelMatrix(bmp);
+                image.save("oldtimey.bmp");
+
+
+        }
+        cout <<"End"<< endl;
+
+        return 0;
 
 }
