fb7566a0a632158eb84c63d3dd118aa9ff61ff8e
ricardo1447 <rflores42@mail.csuchico.edu>
2017-10-16 17:21:56 -0700

    Changes the colors to greyscale

--- main.cpp
+++ main.cpp
@@ -11,16 +11,21 @@ int main()
     vector < vector < Pixel > > bmp;
     Pixel rgb;
 
-    image.open("machupicchu.bmp");
+    cout << "Please state the file name of an image: " << endl;
+    bool validBmp = image.isImage();
+    if(validBmp == true)
+    {
     bmp = image.toPixelMatrix();
 
-
-    cout << "machupicchu.mbp has been loaded. It is " << bmp.size() << " pixels wide and " << bmp[0].size() << " pixels high" << endl;
-
-// Output will request the image file name from the user.
-// Image file must be a Windows BMP format image.
-// Change the pixel colors to black, white, and shades of grey only based on original colors.
-// File must be saved as oldtimey.bmp
+    rgb.red = 0;
+    rgb.blue = 0;
+    rgb.green = 0;
+    
+    bmp[532][2486] = rgb;
+    image.fromPixelMatrix(bmp);
+    image.save("machupicchu.bmp");
+    "machupicchu.bmp" == "oldtimey.bmp";
+    }
     return 0;
 
 }
